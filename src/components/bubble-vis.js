f6dd755f559ccc4d3029516f4108d547381e6e3e
Aditio Pangestu <aditiopangestu@gmail.com>
2018-04-21 18:40:21 +0700

    aggregate data

--- src/components/bubble-vis.js
+++ src/components/bubble-vis.js
@@ -7,24 +7,68 @@ import {
   YAxis,
   VerticalGridLines,
   HorizontalGridLines,
-  MarkSeries
+  VerticalRectSeries,
 } from 'react-vis';
 
 export default class BubbleVis extends Component {
+
+  /*
+    Props
+    data = [datum]
+    datum = {
+      "name": "Eastern Asia",
+      "value": 1.234
+    }
+    highlighted_data = {
+      "name": "Turki",
+      "value": 1.45
+    }
+    xMax = 15
+    first = true
+  */
+
+ constructor(props) {
+   super(props);
+   this.state = {
+     ticks : [{
+       name : "name",
+       value : .5
+     }],
+     preprocessed_data : [
+       {
+         x0: 0,
+         x: 1,
+         y: 3
+       }
+     ]
+   }
+ }
+
   render(){
+    
     return (
       <XYPlot
-        width={this.props.width}
-        height={this.props.height}>
+        yDomain={[0, 2]}
+        width={230}
+        height={75}
+        margin={{ top: 10, bottom: 10 }}
+        stackBy="y">
         <VerticalGridLines />
         <HorizontalGridLines />
-        <XAxis />
         <YAxis />
-        <MarkSeries
-          strokeWidth={2}
-          opacity="0.8"
-          sizeRange={[5, 15]}
-          data={this.props.data} />
+        <VerticalRectSeries
+          data={[
+            { x0: 1, x: 2, y: 10 },
+            { x0: 2, x: 4, y: 5 },
+            { x0: 5, x: 6, y: 15 }
+          ]}
+        />
+        <VerticalRectSeries
+          data={[
+            { x0: 1, x: 2, y: 12 },
+            { x0: 2, x: 4, y: 2 },
+            { x0: 5, x: 6, y: 15 }
+          ]} />
       </XYPlot>
     );
   }
--- src/components/vis.js
+++ src/components/vis.js
@@ -8,8 +8,28 @@ class Vis extends Component {
   constructor(props){
     super(props);
     this.state = { 
-      data : [],
+      data_2015 : {
+        raw : [],
+        agregate : [{
+          data : [{
+            name : "name",
+            value : 0
+          }],
+          max_value : 999,
+          attribute_name : "",
+        }]
+      },
+      data_2016 : {
+
+      },
+      data_2017 : {
+
+      },
+      data : {
+
+      },
       highlighted_data : {},
+      continents : [],
       current_x0_window : 0,
       current_x_window : 0,
       default_x0_window : 0,
@@ -22,14 +42,49 @@ class Vis extends Component {
   }
 
   componentWillMount(){
-    axios.get('./src/data/processed_2015.json')
+    axios.get('./src/data/continents.json')
       .then((response) => {
-        const {data} = response;
+        const continents = response.data.data;
         this.setState({
           ...this.state,
-          data : data
+          continents: continents
         })
-      })
+        axios.get('./src/data/processed_2015.json')
+          .then((response) => {
+            const { data } = response;
+            this.setState({
+              ...this.state,
+              data: this.preproccesData(data, continents)
+            })
+          });
+      });
+  }
+
+  meanContinent(data, atribut, region_name) {
+    return _.meanBy(_.filter(data, (datum) => { return datum.region == region_name}),(datum)=>{
+      return parseFloat(datum[atribut]);
+    })
+  }
+
+  preproccesData(data, continents){
+    var atribut_names = ["generosity", "trust", "freedom", "life_expectancy", "family", "gdp"]
+    var agregates = [];
+    for (var i = 0; i < atribut_names.length;i++){
+      var agregate = {}
+      agregate.attribute_name = atribut_names[i];
+      agregate.data = [];
+      agregate.max_value = parseFloat(_.maxBy(data,(datum)=>{
+        return parseFloat(datum[agregate.attribute_name]);
+      })[agregate.attribute_name]);
+      for(var j = 0; j < continents.length;j++){
+        var continent = {};
+        continent.name = continents[j].name;
+        continent.value = this.meanContinent(data, agregate.attribute_name, continent.name);
+        agregate.data.push(continent);
+      }
+      agregates.push(agregate);
+    }
+    console.log(agregates);
   }
 
   render(){
@@ -37,17 +92,98 @@ class Vis extends Component {
       return (
         <section className="section">
           <div className="columns">
-            <div className="column is-9">
-              <BubbleVis
-                width={this.state.width}
-                height={200}
-                data={_.map(this.state.data,(datum)=>{
-                  return {
-                    x : datum.long,
-                    y : datum.lat,
-                    size: datum.gdp
-                  }
-                })}/>
+            <div className="column is-9"
+              style={{
+                width:"700px"
+              }}>
+              <GeneralMap />
+              <div className="columns is-gapless is-marginless">
+                <div className="column">
+                  <p>Cek</p>
+                  <BubbleVis
+                    first={true}
+                    width={this.state.width}
+                    height={400}
+                    data={_.map(this.state.data,(datum)=>{
+                      return {
+                        x : parseFloat(datum.long),
+                        y: parseFloat(datum.lat),
+                        size: parseFloat(datum.gdp)
+                      }
+                    })}/>
+                </div>
+                <div className="column">
+                  <p>Makan</p>
+                  <BubbleVis
+                    first={true}
+                    width={this.state.width}
+                    height={400}
+                    data={_.map(this.state.data, (datum) => {
+                      return {
+                        x: parseFloat(datum.long),
+                        y: parseFloat(datum.lat),
+                        size: parseFloat(datum.gdp)
+                      }
+                    })} />
+                </div>
+                <div className="column">
+                  <p>Makan</p>
+                  <BubbleVis
+                    first={true}
+                    width={this.state.width}
+                    height={400}
+                    data={_.map(this.state.data, (datum) => {
+                      return {
+                        x: parseFloat(datum.long),
+                        y: parseFloat(datum.lat),
+                        size: parseFloat(datum.gdp)
+                      }
+                    })} />
+                </div>
+              </div>
+              <div className="columns is-gapless">
+                <div className="column">
+                  <p>Cek</p>
+                  <BubbleVis
+                    width={this.state.width}
+                    height={400}
+                    data={_.map(this.state.data,(datum)=>{
+                      return {
+                        x : parseFloat(datum.long),
+                        y: parseFloat(datum.lat),
+                        size: parseFloat(datum.gdp)
+                      }
+                    })}/>
+                </div>
+                <div className="column">
+                  <p>Makan</p>
+                  <BubbleVis
+                    first={false}
+                    width={this.state.width}
+                    height={400}
+                    data={_.map(this.state.data, (datum) => {
+                      return {
+                        x: parseFloat(datum.long),
+                        y: parseFloat(datum.lat),
+                        size: parseFloat(datum.gdp)
+                      }
+                    })} />
+                </div>
+                <div className="column">
+                  <p>Makan</p>
+                  <BubbleVis
+                    first={false}
+                    width={this.state.width}
+                    height={400}
+                    data={_.map(this.state.data, (datum) => {
+                      return {
+                        x: parseFloat(datum.long),
+                        y: parseFloat(datum.lat),
+                        size: parseFloat(datum.gdp)
+                      }
+                    })} />
+                </div>
+              </div>
             </div>
             <div className="column is-3">
               
