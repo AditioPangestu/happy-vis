58b6e854691f6b51c31309894ba190c00e017771
Aditio Pangestu <aditiopangestu@gmail.com>
2018-04-21 21:40:26 +0700

    push

--- src/components/bubble-vis.js
+++ src/components/bubble-vis.js
@@ -30,6 +30,10 @@ export default class BubbleVis extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      ticks : [{
+        name : "name",
+        value : 1.2
+      }],
       preprocessed_data : [
         {
           x0: 0,
@@ -41,15 +45,27 @@ export default class BubbleVis extends Component {
   }
 
   componentWillMount(){
+    const {
+      ticks,
+      preprocessed_data
+    } = this.preprocessedData(this.props.data)
     this.setState({
       ...this.state,
-      preprocessed_data: this.preprocessedData(this.props.data)
+      preprocessed_data: preprocessed_data,
+      ticks: ticks,
     })
   }
 
   preprocessedData(data){
     var preprocessed_data = [];
     var ticks = [];
+    var gap = this.props.y_domain/5;
+    for(var i=0;i<5;i++){
+      var tick = {};
+      tick.name = i*gap;
+      tick.value = i*gap;
+      ticks.push(tick);
+    }
     for(var i=0;i<data.length;i++){
       const datum = data[i];
       preprocessed_data.push({
@@ -59,7 +75,10 @@ export default class BubbleVis extends Component {
         color: datum.color
       });
     }
-    return preprocessed_data;
+    return {
+      ticks,
+      preprocessed_data
+    };
   }
 
   render(){
@@ -74,7 +93,10 @@ export default class BubbleVis extends Component {
         stackBy="y">
         <VerticalGridLines />
         <HorizontalGridLines />
-        <YAxis tickTotal={2}/>
+        <YAxis 
+          tickValue={_.map(this.state.ticks, (tick)=>{return tick.value})}
+          />
+
         <VerticalRectSeries
           data={this.state.preprocessed_data}
         />
